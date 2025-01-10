fdecc780c10e1b7b7258cbf77045a0ba3ee93286
QuliyevMSH <167703400+QuliyevMSH@users.noreply.github.com>
2025-01-11 03:27:51 +0400

    Delete src directory

--- src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-const Index = () => {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="bg-green-500 w-64 h-64 rounded-lg"></div>
-    </div>
-  );
-};
-
-export default Index;
